3871a38a83f69effc4b424099cb3028c1ba1aed9
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2014-11-25 19:38:46 -0800

    Start listening on the hatsnipe socket
    Start of allowing IPC

--- lib/hatsnipe/engineer.rb
+++ lib/hatsnipe/engineer.rb
@@ -1,11 +1,20 @@
+require 'socket'
+
 module Hatsnipe
   class Engineer
     def initialize
+      @socket = UNIXServer.new("/tmp/hatsnipe.sock")
     end
     def nest
-      loop do
-        puts "Snoozing"
-        sleep(5)
+      begin
+        while connection = @socket.accept
+          while command = connection.gets
+            command = command.split(':').first
+            puts command
+          end
+        end
+      rescue SystemExit, Interrupt
+        @socket.close
       end
     end
   end
